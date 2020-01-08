ce224efabdeacf92d56bc50fe7b387e69175404d
tyom-1996 <hakobjanyan.200796@gmail.com>
2020-01-08 17:41:45 +0400

    change 7

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,63 +1,58 @@
 
 
-<footer>
+<footer style="background: url({{asset('images/footer.jpg')}});background-size: cover;background-attachment: fixed">
     <div class="footer-bg">
-
-
         <div class="footer-wrap">
+
             <ul class="footer-ul">
-                <h3 class="footer-ul-li">
-                    ABOUT
-                </h3>
+                <h3 class="footer-ul-li"> ABOUT </h3>
                 <li class="footer-ul-li">
-                    Our mission
+                    <a href="#">Our mission</a>
                 </li>
                 <li class="footer-ul-li">
-                    How we work
+                    <a href="#">How we work</a>
                 </li>
                 <li class="footer-ul-li">
-                    Privacy policy
+                    <a href="#">Privacy policy</a>
                 </li>
                 <li class="footer-ul-li">
-                    Terms of use
+                    <a href="#">Terms of use</a>
                 </li>
             </ul>
 
             <ul class="footer-ul">
-                <h3 class="footer-ul-li">
-                    INDIVIDUALS
-                </h3>
+                <h3 class="footer-ul-li"> INDIVIDUALS </h3>
                 <li class="footer-ul-li">
-                    Create online CV
+                    <a href="#">Create online CV</a>
                 </li>
                 <li class="footer-ul-li">
-                    View all jobs
+                    <a href="#">View all jobs</a>
                 </li>
                 <li class="footer-ul-li">
-                    View all companies
+                    <a href="#">View all companies</a>
                 </li>
+
             </ul>
 
             <ul class="footer-ul">
-                <h3 class="footer-ul-li">
-                    COMPANIES
-                </h3>
+                <h3 class="footer-ul-li"> COMPANIES </h3>
                 <li class="footer-ul-li">
-                    Register as employer
+                    <a href="#">Register as employer</a>
                 </li>
                 <li class="footer-ul-li">
-                    View job packages
+                    <a href="#">View job packages</a>
                 </li>
                 <li class="footer-ul-li">
-                    View all companies
+                    <a href="#">View all companies</a>
                 </li>
+
             </ul>
 
             <div class="subscription-section">
                 <h3 >RECEIVE JOB ALERTS RIGHT ON YOUR MAILBOX</h3>
                 <p>Provide your email address and subscribe</p>
                 <form action="" method="post">
-                    <input id="subscribtion-input" type="email" placeholder="Enter you|">
+                    <input id="subscribtion-input" type="email" placeholder="">
                     <button><i class="fa fa-paper-plane"></i></button>
                 </form>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,8 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('home');
-});
+//
+//Route::get('/', function () {
+//    return view('home');
+//});
+Route::get('/',"index@MainController");
